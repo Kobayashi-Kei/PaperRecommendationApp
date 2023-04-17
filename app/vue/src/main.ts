4575ca1854837c49b7f98bc9b1d647b0fbbedf2d
K-Kobayashi <k1810249@uec.ac.jp>
2023-04-17 22:31:39 +0900

    ロゴとHPのタイトルを変更

--- app/vue/src/main.ts
+++ app/vue/src/main.ts
@@ -4,11 +4,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
